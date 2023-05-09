b9c73020e9c1329444e1a5f4c70f937582f3e96e
JakobDylanC <jakobdylanc@gmail.com>
2023-05-09 02:12:48 -0400

    new commit

--- gpt4_functions.py
+++ gpt4_functions.py
@@ -59,10 +59,15 @@ def split_response(response, max_length):
     
     response_chunks = []
     start_i = 0
+    print('bruh2')
     while start_i < len(response):
+        print('bruh3')
         end_i = min(start_i + max_length, len(response))
+        print('bruh4')
         if end_i < len(response):
+            print('bruh5')
             end_i = (found := response.rfind(" ", start_i, end_i)) if found != -1 else end_i
+            print('bruh6')
         print(f"start_i: {start_i}, end_i: {end_i}, found: {found}")  # Debug print
         response_chunks.append(response[start_i:end_i])
         start_i = end_i + 1 if found != -1 else end_i
