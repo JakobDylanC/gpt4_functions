c888339beaa358e652a821d00e9638d4899143d9
JakobDylanC <jakobdylanc@gmail.com>
2023-05-11 01:34:07 -0400

    cleaning up

--- gpt4_functions.py
+++ gpt4_functions.py
@@ -7,22 +7,14 @@ import asyncio
 if "OPENAI_API_KEY" not in os.environ:
     raise ValueError(f"Required environment variable OPENAI_API_KEY is not set.")
 openai.api_key = os.environ["OPENAI_API_KEY"]
-GPT4_MAX_TOTAL_TOKENS = 8192
 GPT4_TOKENS_PER_MESSAGE = 3
 GPT4_TOKENS_PER_NAME = 1
 GPT4_TOKENS_PER_MESSAGES = 3
-MAX_COMPLETION_TOKENS = 1024 #customize here
+GPT4_MAX_TOTAL_TOKENS = 8192
+MAX_COMPLETION_TOKENS = 1024 #customize here, must be less than GPT4_MAX_TOTAL_TOKENS
 MAX_PROMPT_TOKENS = GPT4_MAX_TOTAL_TOKENS - MAX_COMPLETION_TOKENS - GPT4_TOKENS_PER_MESSAGES
-TIMEOUT_SECONDS = 300
+TIMEOUT_SECONDS = 480
 encoding = tiktoken.get_encoding("cl100k_base")
-
-def count_tokens(msg):
-    num_tokens = GPT4_TOKENS_PER_MESSAGE
-    for key, value in msg.items():
-        num_tokens += len(encoding.encode(value))
-        if key == "name":
-            num_tokens += GPT4_TOKENS_PER_NAME
-    return num_tokens
 
 @backoff.on_exception(backoff.expo, openai.error.RateLimitError)
 async def generate_response(system_prompt, msgs):
@@ -39,24 +31,23 @@ async def generate_response(system_prompt, msgs):
         gpt_response = response["choices"][0]["message"]["content"].strip()
         print(f"GPT response:\n{gpt_response}")
         print(f"Prompt tokens: {response['usage']['prompt_tokens']}  Completion tokens: {response['usage']['completion_tokens']}  Total tokens: {response['usage']['total_tokens']}")
-        # if response["usage"]["prompt_tokens"] > MAX_PROMPT_TOKENS:
-        #     try: msgs.pop(1)
-        #     except IndexError: pass
-
-    # except openai.error.InvalidRequestError:
-    #     if len(msgs) > 1:
-    #         print("Too many prompt tokens, trying again...")
-    #         msgs.pop(0)
-    #         gpt_response = await generate_response(msgs)
     except asyncio.TimeoutError:
         print("Timeout exceeded.")
-
     return gpt_response
+
+
+def count_tokens(msg):
+    num_tokens = GPT4_TOKENS_PER_MESSAGE
+    for key, value in msg.items():
+        num_tokens += len(encoding.encode(value))
+        if key == "name":
+            num_tokens += GPT4_TOKENS_PER_NAME
+    return num_tokens
+
 
 def split_response(response, max_length):
     if len(response) <= max_length:
         return [response]
-    
     response_chunks = []
     start_index = 0
     while start_index < len(response):
@@ -65,5 +56,4 @@ def split_response(response, max_length):
         end_index = split_index if split_index != -1 else end_index
         response_chunks.append(response[start_index:end_index])
         start_index = end_index + 1 if split_index != -1 else end_index
-
     return response_chunks
